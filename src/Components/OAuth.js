7f3369d1b986a382ddd3d9d8b56b6c4d80a8ea1d
Kesav-21 <cgkesav@gmail.com>
2023-06-04 00:14:13 +0530

    ouath firebase

--- src/Components/OAuth.js
+++ src/Components/OAuth.js
@@ -1,9 +1,39 @@
 import React from 'react'
 import {FcGoogle} from 'react-icons/fc';
+import { toast } from 'react-toastify';
+import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
+import {serverTimestamp,doc,getDoc,setDoc} from 'firebase/firestore';
+import {db} from '../firebase';
+import {useNavigate, userNavigate} from 'react-router-dom'
 
 const OAuth = () => {
+  const navigate=useNavigate();
+  const onGoogleAuth=async (e)=>{
+    e.preventDefault()
+    try{
+      const auth=getAuth()
+      const provider=new GoogleAuthProvider()
+      const result=await signInWithPopup(auth,provider)
+      const user=result.user;
+      
+      // check database
+
+      const docRef=doc(db,'users',user.uid)
+      const docSnap=await getDoc(docRef)
+      if(!docSnap.exists()){
+        await setDoc(docRef,{
+          name:user.displayName,
+          email:user.email,
+          timestamp:serverTimestamp()
+        })
+      }
+      navigate('/');
+    }catch(error){
+      toast.error("Couldn't Able to Sign In")
+    }
+  }
   return (
-    <button className='flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out rounded'><FcGoogle className='text-2xl bg-white rounded-full mx-4'/>Continue With Google</button>
+    <button onClick={onGoogleAuth} className='flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out rounded'><FcGoogle className='text-2xl bg-white rounded-full mx-4'/>Continue With Google</button>
   )
 }
 
