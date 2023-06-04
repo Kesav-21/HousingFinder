f84e93b2fb11d924227c1567a3cdbddc648a1c1a
Kesav-21 <cgkesav@gmail.com>
2023-06-04 23:41:30 +0530

    Spinner Component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import logo from '../assets/svg/logo.svg';
 import { useLocation, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
@@ -21,11 +22,11 @@ export default function Header() {
     }
   }
   return (
-    <div className="bg-white border-b shadow-sm sticky top-0 z-50">
+    <div className="bg-white border-b shadow-sm sticky top-0 z-40">
       <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
         <div>
           <img
-            src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg"
+            src={logo}
             alt="logo"
             className="h-5 cursor-pointer"
             onClick={() => navigate("/")}
